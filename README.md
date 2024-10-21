cec199d05583f327626280849f469329838aca10
ShotaTotsuka <shota.totsuka@blueish.co.jp>
2024-10-22 08:39:33 +0900

    add requirements txt

--- README.md
+++ README.md
@@ -7,4 +7,15 @@ LiteLLMのデモリポジトリです。
 - https://github.com/BerriAI/litellm
 - https://docs.litellm.ai/docs/
 
+## セットアップ
 
+```bash
+$ pip install -r requirements.txt
+$ cp .env.example .env
+```
+
+## デモの実行方法
+
+```bash
+$ python main.py
+```
--- main.py
+++ main.py
@@ -1,13 +1,56 @@
 from litellm import completion
 from dotenv import load_dotenv
+# import os
 
 load_dotenv()
 
-openai_response = completion(
+# OpenAI
+# os.environ["OPENAI_API_KEY"] = "your-api-key"
+
+# Anthropic
+# os.environ["ANTHROPIC_API_KEY"] = "your-api-key"
+
+# Vertex AI
+# os.environ["VERTEX_PROJECT"] = "hardy-device-386718"
+# os.environ["VERTEX_LOCATION"] = "us-central1"
+
+# HuggingFace
+# os.environ["HUGGINGFACE_API_KEY"] = "huggingface_api_key"
+
+# Azure
+# os.environ["AZURE_API_KEY"] = ""
+# os.environ["AZURE_API_BASE"] = ""
+# os.environ["AZURE_API_VERSION"] = ""
+
+response = completion(
     model="gpt-4o-mini",
-    messages=[
-        {"content": "こんにちは！お元気ですか？", "role": "user"}
-    ]
+    messages=[{"content": "こんにちは！お元気ですか？", "role": "user"}]
 )
 
-print(openai_response.choices[0].message.content)
+# HuggingFace API -----------------------------------
+# response = completion(
+#     model="huggingface/WizardLM/WizardCoder-Python-34B-V1.0",
+#     messages=[{"content": "Hello, how are you?", "role": "user"}],
+#     api_base="https://my-endpoint.huggingface.cloud"
+# )
+
+# Azure API -----------------------------------
+# response = completion(
+#     "azure/<your_deployment_name>",
+#     messages=[{"content": "Hello, how are you?", "role": "user"}]
+# )
+
+# Ollama -----------------------------------
+# response = completion(
+#     model="ollama/llama2",
+#     messages=[{"content": "Hello, how are you?", "role": "user"}],
+#     api_base="http://localhost:11434"
+# )
+
+# OpenRouter -----------------------------------
+# response = completion(
+#     model="openrouter/google/palm-2-chat-bison",
+#     messages=[{"content": "Hello, how are you?", "role": "user"}],
+# )
+
+print(response.choices[0].message.content)
